d250e8b93228e9b5d135eb40f9b48a5a0b65d64d
skearya <77034153+skearya@users.noreply.github.com>
2026-04-19 19:43:31 -0400

    starting bot stuff

--- bot/src/main.py
+++ bot/src/main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from bot!")
+import asyncio
+from os import environ
+
+import discord
+from discord import Intents
+from discord.ext import commands
+
+TEST_GUILD = discord.Object(id=environ.get("TEST_GUILD", "?"))
+
+
+class CustomBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=commands.when_mentioned,
+            intents=Intents.default(),
+        )
+
+    async def setup_hook(self):
+        await self.load_extension("ext.general")
+
+        self.tree.copy_global_to(guild=TEST_GUILD)
+        await self.tree.sync(guild=TEST_GUILD)
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+
+async def main():
+    async with CustomBot() as bot:
+        await bot.start(environ.get("BOT_TOKEN", "?"))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
