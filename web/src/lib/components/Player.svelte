6f1223fe260c98f1ac98ff8d72f8069b03e86444
skearya <77034153+skearya@users.noreply.github.com>
2026-07-24 20:24:43 -0400

    remove animations initial mobile stuff that might be scrapped

--- web/src/lib/components/Player.svelte
+++ web/src/lib/components/Player.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
 	import type { Data, LiquidsoapMetadata } from '$lib/types';
-	import { onDestroy } from 'svelte';
+	import { onDestroy, type Component } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import Circle from '$lib/icons/Circle.svelte';
 	import Person from '$lib/icons/Person.svelte';
 	import Play from '$lib/icons/Play.svelte';
 	import Volume from '$lib/icons/Volume.svelte';
 	import Avatar from '$lib/components/Avatar.svelte';
-	import { flip, cloneOver } from '$lib/animation';
 	import { getListeners } from '$lib/utils';
 
 	let { stream, history, liquidsoap: metadata, icecast }: Data = $props();
 
-	let leftElement: HTMLElement;
-	let rightElement: HTMLElement;
 	let historyElement: HTMLElement;
 
 	let show = $state(false);
@@ -70,58 +67,6 @@
 		}
 	});
 
-	const easeOut = 'cubic-bezier(0.23, 1, 0.32, 1)';
-
-	let leftElementPos: DOMRect | null = null;
-	let rightElementPos: DOMRect | null = null;
-
-	$effect.pre(() => {
-		void show;
-
-		if (leftElement) leftElementPos = leftElement.getBoundingClientRect();
-		if (rightElement) rightElementPos = rightElement.getBoundingClientRect();
-	});
-
-	$effect(() => {
-		void show;
-
-		if (!leftElementPos || !rightElementPos) return;
-
-		const animations: Animation[] = [];
-
-		animations.push(
-			flip(
-				leftElement,
-				{ from: leftElementPos, to: leftElement.getBoundingClientRect() },
-				{ duration: 500, easing: easeOut }
-			)
-		);
-
-		if (show) {
-			animations.push(
-				rightElement.animate(
-					{ opacity: ['0%', '100%'], scale: ['90%', '100%'] },
-					{ duration: 500, easing: easeOut }
-				)
-			);
-		} else {
-			const rightElementClone = cloneOver(rightElement, rightElementPos);
-			rightElementClone.style.display = 'block';
-
-			const rightElementAnimation = rightElementClone.animate(
-				{ opacity: ['100%', '0%'], scale: '90%' },
-				{ duration: 500, easing: easeOut }
-			);
-
-			rightElementAnimation.addEventListener('finish', () => rightElementClone.remove());
-			rightElementAnimation.addEventListener('cancel', () => rightElementClone.remove());
-
-			animations.push(rightElementAnimation);
-		}
-
-		return () => animations.forEach((a) => a.cancel());
-	});
-
 	let title = $derived(`${metadata?.title} - ${metadata?.artist}`);
 	let trackHistory = $derived(history.toReversed().slice(1));
 
@@ -133,11 +78,11 @@
 	{#if metadata?.cover}<link rel="icon" href={metadata.cover} />{/if}
 </svelte:head>
 
-<main in:fade={{ duration: 125 }} class="mx-auto flex h-screen max-w-360 flex-col gap-8.75">
-	<nav class="flex items-center justify-between px-7.5 pt-8">
+<main in:fade={{ duration: 125 }} class="mx-auto flex max-w-360 flex-col gap-8.75 xl:h-screen">
+	<nav class="flex items-center justify-between px-7.5 pt-8 text-nowrap">
 		{#if metadata?.user}
 			<div class="flex gap-x-3.75">
-				<Avatar username={metadata.user} url={metadata?.avatar} class="size-12.75" />
+				<Avatar username={metadata.user} url={metadata?.avatar} class="hidden size-12.75" />
 				<div>
 					<p class="text-[22px] leading-6.5 tracking-wide text-gray">DJ</p>
 					<p class="text-[22px] leading-6.5 tracking-wide">{metadata?.user}</p>
@@ -154,17 +99,11 @@
 				{/if}
 			{/each}
 		</div>
-		<div class="flex items-center gap-x-3 text-nowrap">
-			<div class="flex gap-x-1.5 rounded-[59px] bg-foreground px-5 py-3.75 text-background">
-				<Person />
-				<p class="text-[22px] leading-6.5 tracking-wide">{getListeners(icecast)} Listeners</p>
-			</div>
-			<div class="flex gap-x-1.5 rounded-[59px] bg-red px-5 py-3.75 text-background">
-				<Circle />
-				<p class="text-[22px] leading-6.5 tracking-wide">{metadata?.mode}</p>
-			</div>
+		<div class="flex items-center gap-x-3">
+			{@render label(Person, `${getListeners(icecast)} Listeners`, `bg-foreground text-background`)}
+			{@render label(Circle, metadata?.mode ?? 'Unknown Mode', `bg-red text-background`)}
 			<button
-				class="group relative flex gap-x-1.5 rounded-[59px] bg-light-gray px-5 py-3.75 text-background"
+				class="group relative hidden gap-x-1.5 rounded-[59px] bg-light-gray px-5 py-3.75 text-background xl:flex"
 			>
 				<Volume />
 				<div class="absolute bottom-0 left-0 h-full w-full translate-y-1/2 opacity-0"></div>
@@ -185,16 +124,9 @@
 	</nav>
 
 	<div
-		class={[
-			'mx-auto flex h-full w-full max-w-288.5 items-start overflow-y-hidden px-7.5',
-			show ? 'justify-between' : 'justify-center'
-		]}
+		class="mx-auto flex w-full flex-col items-center justify-between gap-y-8.75 overflow-hidden px-7.5 xl:h-full xl:w-288.5 xl:flex-row xl:items-start"
 	>
-		<div
-			bind:this={leftElement}
-			class="group z-10 flex w-125.75 flex-col items-stretch will-change-transform"
-			{title}
-		>
+		<div class="group z-10 flex max-w-125.75 flex-col items-stretch" {title}>
 			<p class="mb-1.5 truncate text-[48px] leading-14.25 font-medium">
 				{metadata?.title ?? 'Unknown Title'}
 			</p>
@@ -202,7 +134,7 @@
 				{metadata?.artist ?? 'Unknown Artist'}
 			</p>
 			<button
-				class="relative overflow-hidden rounded-[83px] transition-transform active:scale-95"
+				class="relative overflow-hidden rounded-[80px] transition-transform duration-1000 ease-spring active:scale-95"
 				onclick={() => {
 					show = !show;
 
@@ -235,10 +167,7 @@
 			</button>
 		</div>
 
-		<section
-			bind:this={rightElement}
-			class={['h-full w-108.75 flex-col will-change-transform', show ? 'flex' : 'hidden']}
-		>
+		<section class="relative flex w-full flex-col xl:h-full xl:w-108.75">
 			<p class="mt-3.5 mb-6 leading-4.75 tracking-[-0.02em] text-gray">Previous songs</p>
 			<div
 				bind:this={historyElement}
@@ -251,9 +180,21 @@
 					<p>...</p>
 				{/each}
 			</div>
+			<div
+				class="pointer-events-none absolute bottom-0 left-0 h-16 w-full bg-linear-to-t from-background to-transparent"
+			></div>
 		</section>
 	</div>
 </main>
+
+{#snippet label(Icon: Component, text: string, className: string)}
+	<div
+		class={['flex items-center gap-x-1.5 rounded-[59px] px-4 py-2.5 xl:px-5 xl:py-3.75', className]}
+	>
+		<Icon />
+		<p class="text-[16px] leading-4.75 tracking-wide xl:text-[22px] xl:leading-6.5">{text}</p>
+	</div>
+{/snippet}
 
 {#snippet previous(metadata: LiquidsoapMetadata)}
 	<div class="flex snap-start gap-x-5.5">
