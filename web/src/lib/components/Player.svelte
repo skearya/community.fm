59ca996a972601d8888bad44415180c2bca3bd2b
skearya <77034153+skearya@users.noreply.github.com>
2026-07-24 21:29:41 -0400

    dynamic root font size, fix up mobile

--- web/src/lib/components/Player.svelte
+++ web/src/lib/components/Player.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Data, LiquidsoapMetadata } from '$lib/types';
-	import { onDestroy, type Component } from 'svelte';
+	import { onDestroy } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import Circle from '$lib/icons/Circle.svelte';
 	import Person from '$lib/icons/Person.svelte';
@@ -79,36 +79,48 @@
 </svelte:head>
 
 <main in:fade={{ duration: 125 }} class="mx-auto flex max-w-360 flex-col gap-8.75 xl:h-screen">
-	<nav class="flex items-center justify-between px-7.5 pt-8 text-nowrap">
+	<nav class="flex items-center justify-between gap-y-3.75 px-7.5 pt-8 text-nowrap xl:flex-row">
 		{#if metadata?.user}
 			<div class="flex gap-x-3.75">
-				<Avatar username={metadata.user} url={metadata?.avatar} class="hidden size-12.75" />
+				<Avatar username={metadata.user} url={metadata?.avatar} class="size-12.75" />
 				<div>
-					<p class="text-[22px] leading-6.5 tracking-wide text-gray">DJ</p>
-					<p class="text-[22px] leading-6.5 tracking-wide">{metadata?.user}</p>
+					<p class="text-[1.375rem] leading-6.5 tracking-wide text-gray">DJ</p>
+					<p class="text-[1.375rem] leading-6.5 tracking-wide">{metadata?.user}</p>
 				</div>
 			</div>
 		{/if}
-		<div class="ml-20 flex flex-1 justify-start gap-x-12.5">
+		<div class="ml-20 hidden flex-1 justify-start gap-x-12.5 xl:flex">
 			{#each [['Play Count', metadata?.playcount]] as [name, value] (name)}
 				{#if value}
 					<div>
-						<p class="text-[22px] leading-6.5 tracking-wide text-gray">{name}</p>
-						<p class="text-[22px] leading-6.5 tracking-wide">{value}</p>
+						<p class="text-[1.375rem] leading-6.5 tracking-wide text-gray">{name}</p>
+						<p class="text-[1.375rem] leading-6.5 tracking-wide">{value}</p>
 					</div>
 				{/if}
 			{/each}
 		</div>
 		<div class="flex items-center gap-x-3">
-			{@render label(Person, `${getListeners(icecast)} Listeners`, `bg-foreground text-background`)}
-			{@render label(Circle, metadata?.mode ?? 'Unknown Mode', `bg-red text-background`)}
+			<div
+				class="flex items-center gap-x-1.5 rounded-[3.6875rem] bg-foreground px-5 py-3.75 text-background"
+			>
+				<Person />
+				<p class="text-[1.375rem] leading-6.5 tracking-wide">
+					{getListeners(icecast)} <span class="hidden xl:inline">Listeners</span>
+				</p>
+			</div>
+			<div
+				class="flex items-center gap-x-1.5 rounded-[3.6875rem] bg-red px-5 py-3.75 text-background"
+			>
+				<Circle />
+				<p class="hidden text-[1.375rem] leading-6.5 tracking-wide xl:block">{metadata?.mode}</p>
+			</div>
 			<button
-				class="group relative hidden gap-x-1.5 rounded-[59px] bg-light-gray px-5 py-3.75 text-background xl:flex"
+				class="group relative hidden gap-x-1.5 rounded-[3.6875rem] bg-light-gray px-5 py-3.75 text-background xl:flex"
 			>
 				<Volume />
 				<div class="absolute bottom-0 left-0 h-full w-full translate-y-1/2 opacity-0"></div>
 				<div
-					class="absolute -bottom-2 left-1/2 z-20 hidden -translate-x-1/2 translate-y-full rounded-[59px] bg-light-gray px-3.75 py-5 shadow group-hover:block"
+					class="absolute -bottom-2 left-1/2 z-20 hidden -translate-x-1/2 translate-y-full rounded-[3.6875rem] bg-light-gray px-3.75 py-5 shadow group-hover:block"
 				>
 					<input
 						bind:value={volumeLevel}
@@ -124,17 +136,17 @@
 	</nav>
 
 	<div
-		class="mx-auto flex w-full flex-col items-center justify-between gap-y-8.75 overflow-hidden px-7.5 xl:h-full xl:w-288.5 xl:flex-row xl:items-start"
+		class="mx-auto flex h-full w-full max-w-288.5 flex-col items-center justify-between gap-y-8.75 overflow-hidden px-7.5 xl:flex-row xl:items-start"
 	>
-		<div class="group z-10 flex max-w-125.75 flex-col items-stretch" {title}>
-			<p class="mb-1.5 truncate text-[48px] leading-14.25 font-medium">
+		<div class="group z-10 flex w-full max-w-125.75 min-w-0 flex-col items-stretch" {title}>
+			<p class="mb-1.5 truncate text-[3rem] leading-14.25 font-medium">
 				{metadata?.title ?? 'Unknown Title'}
 			</p>
-			<p class="mb-7.5 truncate text-[32px] leading-9.5 text-gray">
+			<p class="mb-7.5 truncate text-[2rem] leading-9.5 text-gray">
 				{metadata?.artist ?? 'Unknown Artist'}
 			</p>
 			<button
-				class="relative overflow-hidden rounded-[80px] transition-transform duration-1000 ease-spring active:scale-95"
+				class="relative overflow-hidden rounded-[5rem] transition-transform duration-1000 ease-spring active:scale-95"
 				onclick={() => {
 					show = !show;
 
@@ -167,7 +179,7 @@
 			</button>
 		</div>
 
-		<section class="relative flex w-full flex-col xl:h-full xl:w-108.75">
+		<section class="relative flex h-full w-full flex-col xl:w-108.75">
 			<p class="mt-3.5 mb-6 leading-4.75 tracking-[-0.02em] text-gray">Previous songs</p>
 			<div
 				bind:this={historyElement}
@@ -181,20 +193,11 @@
 				{/each}
 			</div>
 			<div
-				class="pointer-events-none absolute bottom-0 left-0 h-16 w-full bg-linear-to-t from-background to-transparent"
+				class="pointer-events-none absolute bottom-0 left-0 hidden h-16 w-full bg-linear-to-t from-background to-transparent xl:block"
 			></div>
 		</section>
 	</div>
 </main>
-
-{#snippet label(Icon: Component, text: string, className: string)}
-	<div
-		class={['flex items-center gap-x-1.5 rounded-[59px] px-4 py-2.5 xl:px-5 xl:py-3.75', className]}
-	>
-		<Icon />
-		<p class="text-[16px] leading-4.75 tracking-wide xl:text-[22px] xl:leading-6.5">{text}</p>
-	</div>
-{/snippet}
 
 {#snippet previous(metadata: LiquidsoapMetadata)}
 	<div class="flex snap-start gap-x-5.5">
@@ -205,7 +208,7 @@
 			class="aspect-square size-25.5 rounded-2xl object-cover"
 		/>
 		<div class="min-w-0 flex-1">
-			<div class="mb-3.5 text-[22px] leading-7.5">
+			<div class="mb-3.5 text-[1.375rem] leading-7.5">
 				<p class="truncate">{metadata.title}</p>
 				<p class="truncate text-gray">{metadata.artist}</p>
 			</div>
